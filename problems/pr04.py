ba0dcd36cbc3cf5764f9e903e472a10efb514a97
Gökhan Öztürk <gokhan@eg-ozturk.com>
2021-12-07 11:52:20 +0300

    adds problem 4 solution

--- problems/pr04.py
+++ problems/pr04.py
@@ -9,4 +9,9 @@ product of two 2-digit numbers is 9009 = 91 × 99.
 Find the largest palindrome made from the product of two 3-digit numbers.
 """
 
+def is_palindrome(num):
+    return str(num) == str(num)[::-1]
 
+digits = [int(f'{i}{j}{k}') for i in range(1, 10) for j in range(10) for k in range(10)]
+
+print(max([i * j for i in digits for j in digits if is_palindrome(i * j)]))
